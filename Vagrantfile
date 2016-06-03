77f936caa900c25863c89f1ddfcc76e7b72093a1
Vincent Chu <vince.chu@live.ca>
2016-06-02 21:50:56 -0400

    refactor(Vagrantfile): move main logic into machine.rb

--- Vagrantfile
+++ Vagrantfile
@@ -5,121 +5,17 @@ require 'yaml'
 require 'json'
 
 root_path = File.expand_path(File.dirname(__FILE__))
-vagrant_yaml_path = root_path + '/vagrant.yml'
-vagrant_json_path = root_path + '/vagrant.json'
-vagrant = if File.exists?(vagrant_yaml_path) then
-            YAML::load(File.read(vagrant_yaml_path))
-          elsif File.exists?(vagrant_json_path) then
-            JSON.parse(File.read(vagrant_json_path))
-          end
+vagrant_yaml_path = "#{root_path}/vagrant.yml"
+vagrant_json_path = "#{root_path}/vagrant.json"
+
+require "#{root_path}/scripts/machine.rb"
 
 Vagrant.configure(2) do |config|
-  vagrant['machines'].each do |machine|
-    config.vm.define machine['name'] do |vmconfig|
-
-      # Configures supported fields for base box
-      # for which the machine will be based on and
-      # inherit from.
-
-      ['box', 'box_url', 'box_version'].each do |key|
-        vmconfig.vm.send(key + '=', machine[key]) if machine.has_key?(key)
-      end # box
-
-      # Configures the machine for each specified provider.
-      # Current supported providers: virtualbox.
-
-      machine['providers'].each do |provider, data|
-        data['name'] = vagrant['project'] + '-' + machine['name'] unless data.has_key?('name')
-        vmconfig.vm.provider provider do |vm|
-          case provider
-            when 'virtualbox' then
-              ['name', 'gui', 'linked_clone', 'cpus', 'memory', 'customize'].each do |key|
-                if data.has_key?(key) then
-                  unless key == 'customize' then
-                    vm.send(key + '=', data[key])
-                  else # special customization
-                    data[key].each do |key, value|
-                      vm.customize ['modifyvm', :id, '--' + key, value]
-                    end
-                  end
-                end
-              end # each key
-          end # case provider
-        end # vmconfig.vm.provider
-      end # providers
-
-      # Configures the SSH properties for how
-      # Vagrant will access your machine over SSH.
-
-      ['username', 'password', 'shell', 'private_key_path', 'insert_key'].each do |key|
-        if machine['ssh'].has_key?(key) then
-          vmconfig.ssh.send(key + '=', machine['ssh'][key])
-        end
-      end # ssh
-
-      # Configures the various metworks the machine
-      # should be able to connect to.
-
-      machine['networks'].each do |net|
-        net = net.inject({}) {|memo, (k, v)| memo[k.to_sym] = v; memo}
-        case net[:kind]
-          when 'forwarded_port' then
-            # do nothing
-          when 'private_network' then
-            if net[:ip] == 'dynamic' then
-              net.delete(:ip)
-              net[:type] = 'dhcp'
-            end
-          when 'public_network' then
-            net.delete(:ip) if net[:ip] == 'dynamic'
-        end
-        vmconfig.vm.network net.delete(:kind), net
-      end # networks
-
-      # Configures synced folders that enable Vagrant to sync a folder
-      # on the host machine to the guest machine, allowing you to
-      # continue working on your project's files on your host machine,
-      # but use the resources in the guest machine to compile or run
-      # your project.
-
-      machine['synced_folders'].each do |sf|
-        vmconfig.vm.synced_folder sf['host'], sf['guest'] do |sfconfig|
-          ['create', 'group', 'owner', 'mount_options'].each do |key|
-            sfconfig.send(key + '=', sf[key]) if sf.has_key?(key)
-          end
-        end # vmconfig.vm.synced_folder
-      end # synced_folders
-
-      # Configures and executes the specified provisioning
-      # rules and routines that specialize the machine.
-      # Current provisioner supported: file, shell
-
-      machine['provisions'].each do |p|
-        p['run'] = 'once' unless p.has_key?('run')
-        if p['kind'] == 'file' then
-          if not File.exists?(p['source']) then
-            next # does not exist, exit
-          end
-          if p.has_key?('target') and not p.has_key?('destination') then
-            p['destination'] = p.delete('target') + '/' + File.basename(p['source'])
-          end
-        end
-        vmconfig.vm.provision p['name'], type: p['kind'], run: p['run'] do |pconfig|
-          case p['kind']
-            when 'file' then
-              ['source', 'destination']
-            when 'shell' then
-              ['inline', 'path', 'privileged', 'args', 'env', 'upload_path', 'binary']
-            else
-              []
-          end.each do |key|
-            if p.has_key?(key) then
-              pconfig.send(key + '=', p[key])
-            end
-          end
-        end # vmconfig.vm.provision
-      end # provisions
-
-    end # config.vm.define
-  end # machines
+  Machine.configure(config, if File.exists?(vagrant_yaml_path) then
+    YAML::load(File.read(vagrant_yaml_path))
+  elsif File.exists?(vagrant_json_path) then
+    JSON.parse(File.read(vagrant_json_path))
+  else
+    raise 'Cannot find vagrant.yml or vagrant.json.'
+  end)
 end # Vagrant.configure
---
+++ scripts/machine.rb
@@ -0,0 +1,215 @@
+#--------------------------------------------------
+# machine.rb
+#
+# Represents each virtual machine and the
+# associated machine configurations and the
+# virtual machine provisioner settings.
+#--------------------------------------------------
+class Machine
+
+  ## Class methods
+
+  # Recursively, convert hash keys from string keys
+  # to symbol keys. Returns the newly transformed
+  # object.
+  def Machine.key_to_sym(value)
+    if value.is_a?(Array) then
+      value.map {|v| Machine.key_to_sym(v)}
+    elsif value.is_a?(Hash) then
+      value.inject({}) {|memo, (k, v)| memo[k.to_sym] = Machine.key_to_sym(v); memo}
+    else
+      value
+    end
+  end
+
+  # Configures the machines with the given settings
+  def Machine.configure(config, settings)
+    settings = Machine.key_to_sym(settings)
+    unless settings.nil? then
+      settings[:machines].each do |machine|
+        config.vm.define machine[:name] do |cnf|
+          m = Machine.new(cnf, machine, settings)
+          ['box', 'ssh', 'providers', 'network', 'synced_folders', 'provision'].each do |key|
+            m.send("config_#{key}")
+          end
+        end
+      end
+    end
+  end
+
+  ## Instance methods
+
+  # Constructs and initializes a new Machine
+  # instance with the given VM configuration,
+  # machine data and provisioning settings
+  def initialize(config, machine, settings)
+    @name     = "#{settings[:project]}-#{machine[:name]}"
+    @config   = config
+    @machine  = machine
+    @settings = settings
+  end
+
+  # Configures supported fields for base box
+  # for which the machine will be based on and
+  # inherit from.
+  def config_box()
+    [:box, :box_url, :box_version].each do |k|
+      @config.vm.send("#{k.to_s}=", @machine[k]) if @machine.has_key?(k)
+    end
+  end
+
+  # Configures the SSH properties for how
+  # Vagrant will access your machine over SSH.
+  def config_ssh()
+    [:username, :password, :shell, :private_key_path, :insert_key].each do |k|
+      @config.ssh.send("#{k.to_s}=", @machine[:ssh][k]) if @machine[:ssh].has_key?(k)
+    end
+  end
+
+  ## Provider Configurations
+
+  # Configures the machine for each specified provider.
+  # Current supported providers: virtualbox, vmware_fusion,
+  # vmware_workstation and parallels.
+  def config_providers()
+    @machine[:providers].each do |name, config|
+      config[:name] ||= @name
+      @config.vm.provider name do |vm|
+        case name
+          when :virtualbox then config_virtualbox(vm, config)
+          when :vmware_fusion, :vmware_workstation then config_vmware(vm, config)
+          when :parallels then config_parallels(vm, config)
+          when :all then
+            ['virtualbox', 'vmware', 'parallels'].each do |hypervisor|
+              self.send("config_#{hypervisor}", vm, config)
+            end
+          else raise "Unrecognized provider: #{name.to_s}"
+        end
+      end
+    end
+  end
+
+  # Configures the machine for the virtualbox provider.
+  def config_virtualbox(vb, config)
+    config.each do |key, value|
+      case key
+        when :name, :gui, :linked_clone, :cpus, :memory then vb.send("#{key.to_s}=", value)
+        when :customize then value.each {|k, v| vb.customize(['modifyvm', :id, "--#{k.to_s}", v]) }
+        when :vbmanage  then value.each {|v|    vb.customize(v.map {|arg| :id if arg == ':id' else arg }) }
+        else raise "Bad Virtualbox '#{key.to_s}' configuration."
+      end
+    end
+  end
+
+  # Configures the machine for the vmware provider.
+  def config_vmware(vmware, config)
+    config.each do |k, v|
+      vmware.vmx[case k
+        when :name   then 'displayName'
+        when :memory then 'memsize'
+        when :cpus   then 'numvcpus'
+        when :ostype then 'guestOS'
+        else k.to_s
+      end] = (case k
+        when k == :ostype then v.gsub(/_/, '-')
+        else v
+      end)
+    end
+  end
+
+  # Configures the machine for the parallels provider.
+  def config_parallels(para, config)
+    config.each do |k, v|
+      para.send("#{k.to_s}=", v)
+    end
+  end
+
+  ## Network Configurations
+
+  # Configures the various metworks the machine
+  # should be able to connect to.
+  def config_network()
+    @machine[:networks].each do |net|
+      @config.vm.network net[:kind], (case net.delete(:kind)
+        when 'forwarded_port'  then config_net_fwport(net)
+        when 'private_network' then config_net_private(net)
+        when 'public_network'  then config_net_public(net)
+      end)
+    end
+  end
+
+  # Configures the port forwarding for the machine.
+  def config_net_fwport(net)
+    return net
+  end
+
+  # Configures the private network for the machine.
+  def config_net_private(net)
+    if net[:ip] == 'dynamic' then
+      net.delete(:ip)
+      net[:type] = 'dhcp'
+    end
+    return net
+  end
+
+  # Configures the public network for the machine.
+  def config_net_public(net)
+    net.delete(:ip) if net[:ip] == 'dynamic'
+    return net
+  end
+
+  # Configures synced folders that enable Vagrant to sync a folder
+  # on the host machine to the guest machine, allowing you to
+  # continue working on your project's files on your host machine,
+  # but use the resources in the guest machine to compile or run
+  # your project.
+  def config_synced_folders()
+    @machine[:synced_folders].each do |sf|
+      @config.vm.synced_folder sf[:host], sf[:guest] do |config|
+        [:create, :group, :owner, :mount_options].each do |key|
+          config.send("#{key.to_s}=", sf[key]) if sf.has_key?(key)
+        end
+      end
+    end
+  end
+
+  # Configures and executes the specified provisioning
+  # rules and routines that specialize the machine.
+  # Current provisioner supported: file, shell
+  def config_provision()
+    @machine[:provisions].each do |config|
+      kind = config.delete(:kind)
+      config[:run] = 'once' unless config.has_key?(:run)
+      case kind
+        when 'shell' then # do nothing
+        when 'file'  then
+          next unless File.exists?(config[:source])
+          if config.has_key?(:target) and not config.has_key?(:destination) then
+            config[:destination] = config.delete(:target) + '/' + File.basename(config[:source])
+          end
+        else raise "Unrecognized provision '#{kind}'."
+      end
+      @config.vm.provision config[:name], type: kind, run: config[:run] do |p|
+        case kind
+          when 'file'  then config_provision_file(p, config)
+          when 'shell' then config_provision_shell(p, config)
+        end
+      end
+    end
+  end
+
+  # Configures file provisioning with the given configs
+  def config_provision_file(p, config)
+    [:source, :destination].each do |key|
+      p.send("#{key.to_s}=", config[key]) if config.has_key?(key)
+    end
+  end
+
+  # Configures shell provisioning with the given configs
+  def config_provision_shell(p, config)
+    [:inline, :path, :privileged, :args, :env, :upload_path, :binary].each do |key|
+      p.send("#{key.to_s}=", config[key]) if config.has_key?(key)
+    end
+  end
+
+end # Machine
