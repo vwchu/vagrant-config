455e29858638c679e45bd7ced99fb8b8081dfc01
Vincent Chu <vince.chu@live.ca>
2016-09-02 02:14:03 -0400

    chore: cleanup trailing whitespaces

--- scripts/machine.rb
+++ scripts/machine.rb
@@ -71,7 +71,7 @@ class Machine
 
   ## Instance methods
 
-  # Constructs a new Machine instance with the 
+  # Constructs a new Machine instance with the
   # given machine data and provisioning settings.
   def initialize(machine, settings)
     @name     = "#{ENV['VAGRANT_PROJECT_NAME'] ||= settings[:project]}--#{machine[:name]}"
@@ -79,10 +79,10 @@ class Machine
     @settings = settings
   end
 
-  # Configures a Vagrant machine with the given 
+  # Configures a Vagrant machine with the given
   # Vagrant configuration object.
   def config_vm(config)
-    config.vm.define @machine[:name], primary: @machine[:primary], autostart: @machine[:autostart] do |cnf| 
+    config.vm.define @machine[:name], primary: @machine[:primary], autostart: @machine[:autostart] do |cnf|
       @config = cnf # For access within other methods.
       ['box', 'ssh', 'providers', 'network', 'synced_folders', 'provision'].each do |key|
         self.send("config_#{key}")
